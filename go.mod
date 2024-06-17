3a71f4e5c4e34b2cf874fc3674136e886850f4b3
winlin <winlinvip@gmail.com>
2024-06-17 17:41:10 +0800

    Fix #11: Add RTMP example.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ossrs/go-oryx-lib
+
+go 1.4.0
--- rtmp/example_test.go
+++ rtmp/example_test.go
@@ -20,3 +20,69 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 package rtmp_test
+
+import (
+	"fmt"
+	"math/rand"
+	"net"
+	"time"
+
+	"github.com/ossrs/go-oryx-lib/rtmp"
+)
+
+func ExampleRtmpClientHandshake() {
+	// Connect to RTMP server via TCP client.
+	c, err := net.DialTCP("tcp", nil, &net.TCPAddr{IP: net.ParseIP("127.0.0.1"), Port: 1935})
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+
+	rd := rand.New(rand.NewSource(time.Now().UnixNano()))
+	hs := rtmp.NewHandshake(rd)
+
+	// Client send C0,C1
+	if err := hs.WriteC0S0(c); err != nil {
+		panic(err)
+	}
+	if err := hs.WriteC1S1(c); err != nil {
+		panic(err)
+	}
+
+	// Receive S0,S1,S2 from RTMP server.
+	if _, err = hs.ReadC0S0(c); err != nil {
+		panic(err)
+	}
+	s1, err := hs.ReadC1S1(c)
+	if err != nil {
+		panic(err)
+	}
+	if _, err = hs.ReadC2S2(c); err != nil {
+		panic(err)
+	}
+
+	// Client send C2
+	if err := hs.WriteC2S2(c, s1); err != nil {
+		panic(err)
+	}
+}
+
+func ExampleRtmpClientConnect() {
+	// Connect to RTMP server via TCP, then finish the RTMP handshake see ExampleRtmpClientHandshake.
+	var c *net.TCPConn
+
+	// Create a RTMP client.
+	client := rtmp.NewProtocol(c)
+
+	// Send RTMP connect tcURL packet.
+	connectApp := rtmp.NewConnectAppPacket()
+	connectApp.CommandObject.Set("tcUrl", amf0.NewString(fmt.Sprintf("rtmp://%v%v", u.Hostname(), app)))
+	if err = client.WritePacket(connectApp, 1); err != nil {
+		panic(err)
+	}
+
+	var connectAppRes *rtmp.ConnectAppResPacket
+	if _, err = client.ExpectPacket(&connectAppRes); err != nil {
+		panic(err)
+	}
+}
